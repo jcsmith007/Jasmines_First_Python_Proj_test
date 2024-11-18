99b63a48d6b6b81b3b0241dc41ad4a2e11d32046
jcsmith007 <bluginger28@gmail.com>
2024-11-17 20:51:24 -0500

    commit

---
+++ JasminesFirsTest.py
@@ -0,0 +1,46 @@
+print("What's up, People?!")
+if 5 > 2:
+    print("Five is greater than two!")
+    x = 5
+    y = "Jasmine"
+    print(x)
+    print(y)
+    print(5-3)
+    print(87 - 612)
+    print("apple" + "jacks")
+    my_address = 3513
+    print(my_address)
+    name = "jasmine"
+    print(name)
+price = 20
+amount = 113
+total = price * amount
+print(total)
+user_password = "grey goose"
+print(user_password)
+
+greeting = "Whats up "
+name = "Jasmine"
+print(greeting + name)
+#Jasmine makes $77 an hour
+hourly_wage = 77
+#Jasmine works 40 hours a week
+hours_per_week = 40
+#to find weekly income hours and wage are multiplied
+income_per_week = hourly_wage * hours_per_week
+#Jasmine works 48 weeks a year
+weeks_per_year = 48
+#The wks per year nad the income per wk are multiplied to get income per year
+income_per_year = weeks_per_year * income_per_week
+#For the total of income Jasmine has in a year
+print(name + "'s yearly income is:")
+print(income_per_year)
+#Jasmine also pays 1900 a month for rent and food for 52 weeks of the year.
+spend_per_month = 1900
+spend_per_year = spend_per_month * 52
+print(name + "'s yearly spend is:")
+print(spend_per_year)
+#calculate the yearly savings
+savings_per_year = income_per_year - spend_per_year
+print(name + "'s yearly savings is:")
+print(savings_per_year)
